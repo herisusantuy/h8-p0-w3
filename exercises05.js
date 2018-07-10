badf5a79ef20806fc4c7cfa03044a01cc1bd1db2
javasusanto <herisusanto.id@gmail.com>
2018-07-10 17:53:49 +0700

    Exercises 05 Week 3

---
+++ exercises05.js
@@ -0,0 +1,20 @@
+// Logic Challenge - Palindrome
+function palindrome(kata) {
+  var katabaru =''
+  for (i=kata.length-1 ; i >= 0 ; i--){
+         katabaru += kata[i]
+       }
+  if (katabaru===kata){
+    return true
+  }
+  else {
+    return false}
+
+}
+
+// TEST CASES
+console.log(palindrome('katak')); // true
+console.log(palindrome('blanket')); // false
+console.log(palindrome('civic')); // true
+console.log(palindrome('kasur rusak')); // true
+console.log(palindrome('mister')); // false
