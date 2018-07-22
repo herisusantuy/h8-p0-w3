797e4c122746fd538c535dd90780cef48357da63
javasusanto <herisusanto.id@gmail.com>
2018-07-22 11:40:16 +0700

    Revisi logic conditional if utk yg angka ganjil

--- exercises14.js
+++ exercises14.js
@@ -22,13 +22,16 @@ function mengelompokkanAngka(arr) {
     //  console.log (arr[i])
      if (arr[i]%2 === 0 && arr[i]%3 !==0 ){
         arrGenap.push(arr[i])
+        // jika genap
      }
-     else if (arr[i]%2 !== 0){
+     else if (arr[i]%2 !== 0 && arr[i]%3 !=0){
         arrGanjil.push(arr[i])
+        // jika ganjil
      }
      else if (arr[i]%3 === 0){
         arrKelipatan.push(arr[i])
      }
+    //  jika kelipatan 3
     //  arrResult = arrGenap + arrGanjil + arrKelipatan
  }
 arrResult=[arrGenap,arrGanjil,arrKelipatan]
@@ -40,7 +43,7 @@ return arrResult
 }
 
 // TEST CASES
-console.log(mengelompokkanAngka([2, 4, 6])); // [ [2, 4], [], [6] ]
+console.log(mengelompokkanAngka([3,6, 9])); // [ [2, 4], [], [6] ]
 console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
 console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
 console.log(mengelompokkanAngka([])); // [ [], [], [] ]
